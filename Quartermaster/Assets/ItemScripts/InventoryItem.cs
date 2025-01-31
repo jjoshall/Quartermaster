1504c6feed3d8cd583f3716831d408a5c34f1453
Nz <evoxtemp@gmail.com>
2025-01-31 14:50:32 -0800

    pass in user object to inventoryItem use()

--- Quartermaster/Assets/ItemScripts/InventoryItem.cs
+++ Quartermaster/Assets/ItemScripts/InventoryItem.cs
@@ -5,5 +5,5 @@ public abstract class InventoryItem
     // define an int itemID that all subclasses should define as a constant
     public abstract int itemID {get; set;}
 
-    public abstract void use();
+    public abstract void use(GameObject user);
 }
--- Quartermaster/Assets/ItemScripts/MedKit.cs
+++ Quartermaster/Assets/ItemScripts/MedKit.cs
@@ -5,7 +5,7 @@ public class MedKit : InventoryItem
     // define itemID as 1
     // write setter to take a parameter
     public override int itemID { get; set; }
-    public override void use()
+    public override void use(GameObject user)
     {
         Debug.Log("MedKit used");
         // destroy self, replace with empty slot.
