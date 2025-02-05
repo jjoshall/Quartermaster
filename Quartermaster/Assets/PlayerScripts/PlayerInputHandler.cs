32cbec28e69129019eaf09b8801a92b14c6c9b42
AniruddhSin <146397992+AniruddhSin@users.noreply.github.com>
2025-02-05 14:02:18 -0800

    small update before rebase

--- Quartermaster/Assets/PlayerScripts/PlayerInputHandler.cs
+++ Quartermaster/Assets/PlayerScripts/PlayerInputHandler.cs
@@ -1,31 +1,38 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+
+/*
+USE CALLBACKS TO SET VARIABLES, HAVE {GET; PRIVATE SET} FOR DATA VALUES TO BE PASSED TO CONTROLLER WHICH HAS ACTUAL UPDATE()
+*/
 public class PlayerInputHandler : MonoBehaviour
 {
-    //[SerializeField] private PlayerControls playerInput;
+    public Vector2 move_vector { get; private set;}
+    public Vector2 look_vector { get; private set;}
+    public bool jumped {get; private set;}
+    
 
     void Start(){
         //playerInput = new PlayerControls();
+        move_vector = Vector2.zero;
+        look_vector = Vector2.zero;
+        jumped = false;
     }
 
     public void Move(InputAction.CallbackContext ctx){
-        Vector2 move_vector = ctx.ReadValue<Vector2>();
-        Debug.Log("move input detected");
-        Debug.Log(move_vector);
-        //return move_vector;
+        move_vector = ctx.ReadValue<Vector2>();
     }
 
     public void Jump(InputAction.CallbackContext ctx){
         if (ctx.performed){
-            Debug.Log("Jumped");
+            jumped = true;
+        }
+        if (ctx.canceled){
+            jumped = false;
         }
     }
 
     public void Look(InputAction.CallbackContext ctx){
-        Vector2 look_vector = ctx.ReadValue<Vector2>();
-        Debug.Log("look input detected");
-        Debug.Log(look_vector);
-        //return look_vector;
+        look_vector = ctx.ReadValue<Vector2>();
     }
 }
