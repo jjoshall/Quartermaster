a0ae2202ab8e5b7c0dd304ad2a1b81bc85402d76
Josh Hall <148017202+jjoshall@users.noreply.github.com>
2025-02-04 23:31:38 -0800

    boid enemy done
    we can maybe make these the flying and shooting enemies?

--- Quartermaster/Assets/EnemyScripts/EnemyController.cs
+++ Quartermaster/Assets/EnemyScripts/EnemyController.cs
@@ -1,4 +1,6 @@
 // Code help from: https://www.youtube.com/watch?v=Srg6GRFspps&ab_channel=Eincode
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
@@ -7,14 +9,21 @@ public class EnemyController : MonoBehaviour
      [SerializeField] private float rotationSpeed = 10f;
      [SerializeField] private float stoppingDistance = 1.5f;
 
+     [Header("Boids")]
+     [SerializeField] private float detectionDistance = 1f;
+     [SerializeField] private float separationWeight = 1f;
+     [SerializeField] private float alignmentWeight = 1f;
+     [SerializeField] private float cohesionWeight = 1f;
+
      // This is when we get the model of the enemy
      //private Animator animator;
 
      private Transform target;
-     private Vector3 direction = Vector3.zero;
+     private Vector3 direction;
      private Quaternion targetRotation;
 
      private float movementSpeedBlend;
+     private Vector3 separationForce;
 
      private void Awake()
      {
@@ -32,8 +41,19 @@ public class EnemyController : MonoBehaviour
 
      private void FollowTarget()
      {
-          direction = (target.position - transform.position).normalized;
+          separationForce = Vector3.zero;
+          direction = (target.position - transform.position);
+          var newVector = new Vector3(direction.x, 0, direction.z);
           float distance = direction.magnitude;
+
+          var neighbors = GetNeighbors();
+
+          if (neighbors.Length > 0)
+          {
+               CalculateSeparationForce(neighbors);
+               ApplyAllignment(neighbors);
+               ApplyCohesion(neighbors);
+          }
 
           if (distance > stoppingDistance)
           {
@@ -47,19 +67,66 @@ public class EnemyController : MonoBehaviour
           RotateTowardsTarget();
      }
 
+     private void ApplyCohesion(Collider[] neighbors)
+     {
+          Vector3 averagePosition = Vector3.zero;
+          
+          foreach (var neighbor in neighbors)
+          {
+               averagePosition += neighbor.transform.position;
+          }
+
+          averagePosition /= neighbors.Length;
+          Vector3 cohesionDir = (averagePosition - transform.position).normalized;
+          separationForce += cohesionDir * cohesionWeight;
+     }
+
+     private void ApplyAllignment(Collider[] neighbors)
+     {
+          Vector3 neighborsForward = Vector3.zero;
+
+          foreach (var neighbor in neighbors)
+          {
+               neighborsForward += neighbor.transform.forward;
+          }
+
+          if (neighborsForward != Vector3.zero)
+          {
+               neighborsForward.Normalize();
+          }
+
+          separationForce += neighborsForward * alignmentWeight;
+     }
+
+     private void CalculateSeparationForce(Collider[] neighbors)
+     {
+          foreach(var neighbor in neighbors)
+          {
+               var dir = neighbor.transform.position - transform.position;
+               var distance = dir.magnitude;
+               var away = -dir.normalized;
+
+               if (distance > 0)
+               {
+                    separationForce += (away / distance) * separationWeight;
+               }
+          }
+     }
+
+     private Collider[] GetNeighbors()
+     {
+          var enemyMask = LayerMask.GetMask("Enemy");
+          return Physics.OverlapSphere(transform.position, detectionDistance, enemyMask);
+     }
+
      private void MoveTowardsTarget()
      {
           
           direction = direction.normalized;
-          Vector3 movement = direction * speed * Time.deltaTime;
-          transform.position += transform.forward * movement.magnitude;
-          movementSpeedBlend = Mathf.Lerp(movementSpeedBlend, 1, Time.deltaTime * speed);
-          //animator.SetFloat("Speed", movementSpeedBlend);
-     }
-
-     private void StopMove()
-     {
-          movementSpeedBlend = Mathf.Lerp(movementSpeedBlend, 0, Time.deltaTime * speed);
+          var combinedDirection = (direction + separationForce).normalized;
+          var movement = combinedDirection * speed * Time.deltaTime;
+          transform.position += movement;
+          //movementSpeedBlend = Mathf.Lerp(movementSpeedBlend, 1, Time.deltaTime * speed);
           //animator.SetFloat("Speed", movementSpeedBlend);
      }
 
@@ -67,5 +134,12 @@ public class EnemyController : MonoBehaviour
      {
           targetRotation = Quaternion.LookRotation(direction);
           transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+     }
+
+     private void StopMove()
+     {
+          Debug.Log("Stop");
+          //movementSpeedBlend = Mathf.Lerp(movementSpeedBlend, 0, Time.deltaTime * speed);
+          //animator.SetFloat("Speed", movementSpeedBlend);
      }
 }
