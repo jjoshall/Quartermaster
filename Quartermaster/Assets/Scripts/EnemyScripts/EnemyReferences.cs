36396e9337d44e1aedf376e7d6de0d19dd422423
Josh Hall <148017202+jjoshall@users.noreply.github.com>
2025-02-11 13:23:00 -0800

    made shader inactive at start

--- Quartermaster/Assets/Scripts/EnemyScripts/EnemyReferences.cs
+++ Quartermaster/Assets/Scripts/EnemyScripts/EnemyReferences.cs
@@ -8,11 +8,14 @@ public class EnemyReferences : MonoBehaviour
      // [HideInInspector] public Animator animator;
 
      [Header("Stats")]
-     public float pathUpdateDelay = 0.2f;
+     // Path will update at a random float time between 0.2 - 0.5 seconds so that all enemies don't update at the same time causing performance issues
+     public float pathUpdateDelay;
 
      private void Awake()
      {
           agent = GetComponent<NavMeshAgent>();
           // animator = GetComponent<Animator>();
+
+          pathUpdateDelay = Random.Range(0.2f, 0.5f);
      }
 }
