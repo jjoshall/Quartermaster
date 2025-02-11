c1b4e2ed06ab79eff175e007c7e89cb3ae5a116b
Josh Hall <148017202+jjoshall@users.noreply.github.com>
2025-02-11 14:20:51 -0800

    melee enemies spawning

---
+++ Quartermaster/Assets/Scripts/EnemyScripts/EnemySpawner.cs
@@ -0,0 +1,36 @@
+using Unity.Netcode;
+using UnityEngine;
+
+public class EnemySpawner : NetworkBehaviour
+{
+     [SerializeField] private GameObject enemyPrefab;
+     [SerializeField] private int maxEnemyInstanceCount = 10;
+
+     private void Awake()
+     {
+          // initial pool
+     }
+
+     public void SpawnEnemies()
+     {
+          if (!IsServer)
+          {
+               return;
+          }
+
+          for (int i = 0; i < maxEnemyInstanceCount; i++)
+          {
+               GameObject enemy = Instantiate(enemyPrefab, new Vector3(Random.Range(-10, 10), 3, Random.Range(-10, 10)), Quaternion.identity);
+               
+               // pool instantiation
+          }
+     }
+
+     private void Update()
+     {
+          if (Input.GetKeyDown(KeyCode.M))
+          {
+               SpawnEnemies();
+          }
+     }
+}
