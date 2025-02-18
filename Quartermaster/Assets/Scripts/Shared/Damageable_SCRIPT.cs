2726b7da30c33594c895bc0e0e3292f6cef5a422
AniruddhSin <146397992+AniruddhSin@users.noreply.github.com>
2025-02-18 12:41:15 -0800

    added health and damageable logic scripts

---
+++ Quartermaster/Assets/Scripts/Shared/Damageable_SCRIPT.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class Damageable : MonoBehaviour
+{
+    [Tooltip("Multiplier to apply to the received damage")]
+    public float DamageMultiplier = 1f;
+
+    [Range(0, 1)] [Tooltip("Multiplier to apply to self damage")]
+    public float SensibilityToSelfdamage = 0.1f;
+
+    public Health Health { get; private set; }
+
+    void Awake()
+    {
+        // find the health component either at the same level, or higher in the hierarchy
+        Health = GetComponent<Health>();
+        if (!Health)
+        {
+            Health = GetComponentInParent<Health>();
+        }
+    }
+
+    public void InflictDamage(float damage, bool isExplosionDamage, GameObject damageSource)
+    {
+        if (Health)
+        {
+            var totalDamage = damage;
+
+            // explosive damage does not crit
+            if (!isExplosionDamage)
+            {
+                totalDamage *= DamageMultiplier;
+            }
+
+            // self inflicted damage is lowered
+            if (Health.gameObject == damageSource)
+            {
+                totalDamage *= SensibilityToSelfdamage;
+            }
+
+            // apply the damages
+            Health.TakeDamage(totalDamage, damageSource);
+        }
+    }
+}
