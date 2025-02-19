f93858e3ae1186616d317cf03a9b99829ae9b39d
AniruddhSin <146397992+AniruddhSin@users.noreply.github.com>
2025-02-19 13:42:43 -0800

    enemy health indicator
    made color of enemies scale with their hp (until world space hp bars are available)

--- Quartermaster/Assets/Scripts/EnemyScripts/EnemyHealth_SCRIPT.cs
+++ Quartermaster/Assets/Scripts/EnemyScripts/EnemyHealth_SCRIPT.cs
@@ -1,14 +1,21 @@
 using UnityEngine;
 using Unity.Netcode;
 
+[RequireComponent(typeof(Health) , typeof(Renderer))]
 public class EnemyHealth : NetworkBehaviour
 {
 
     public Health health;
+    public Renderer renderer;
     void Start(){
+        if (!renderer) renderer = GetComponent<Renderer>();
         if (!health) health = GetComponent<Health>();
         health.OnDamaged += OnDamaged;
         health.OnDie += OnDie;
+    }
+
+    void Update(){
+        renderer.material.color = new Color(health.GetRatio(), 0f, 0f);
     }
     void OnDamaged(float damage, GameObject source){
           Debug.Log("enemy took damage");
