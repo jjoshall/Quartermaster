fed994b48bef03602ac4bd0b55629ee49be91cef
Nathan Shturm <exterminathan@gmail.com>
2025-02-21 02:07:51 -0800

    Edited Lobby UI to match visual style

--- Quartermaster/Assets/Scripts/UIScripts/LobbyManagerUI_SCRIPT.cs
+++ Quartermaster/Assets/Scripts/UIScripts/LobbyManagerUI_SCRIPT.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using System.Collections;
+using UnityEngine.EventSystems;
 
 public class LobbyManagerUI : MonoBehaviour {
     [SerializeField] private Button createRelayBtn;
@@ -16,8 +17,6 @@ public class LobbyManagerUI : MonoBehaviour {
         createRelayBtn.onClick.AddListener(() => {
             IPRelay.CreateRelay();
             StartCoroutine(HideLobbyUI());
-
-
         });
 
         joinRelayInput.onEndEdit.AddListener((string s) => {
@@ -27,6 +26,8 @@ public class LobbyManagerUI : MonoBehaviour {
                 HideLobbyUI();
             }
         });
+
+        AddHoverEffect(createRelayBtn);
     }
 
     public IEnumerator HideLobbyUI() {
@@ -36,5 +37,31 @@ public class LobbyManagerUI : MonoBehaviour {
         yield return new WaitUntil(() => Camera.main != null && Camera.main.enabled);
         
         yield return new WaitForEndOfFrame();
+    }
+
+    private void AddHoverEffect(Button button) {
+        TextMeshProUGUI buttonText = button.GetComponentInChildren<TextMeshProUGUI>();
+
+        if (buttonText == null) {
+            Debug.LogWarning($"No TextMeshProUGUI found in {button.name}'s children.");
+            return;
+        }
+
+        EventTrigger trigger = button.gameObject.AddComponent<EventTrigger>();
+
+        EventTrigger.Entry entryEnter = new EventTrigger.Entry();
+        entryEnter.eventID = EventTriggerType.PointerEnter;
+        entryEnter.callback.AddListener((data) => { ChangeTextColor(buttonText, Color.white); });
+
+        EventTrigger.Entry entryExit = new EventTrigger.Entry();
+        entryExit.eventID = EventTriggerType.PointerExit;
+        entryExit.callback.AddListener((data) => { ChangeTextColor(buttonText, Color.black); });
+
+        trigger.triggers.Add(entryEnter);
+        trigger.triggers.Add(entryExit);
+    }
+
+    private void ChangeTextColor(TextMeshProUGUI text, Color color) {
+        text.color = color;
     }
 }
