5648d286d244dd5ed53245a7f5e5e16c4c1ff0b8
Nz <evoxtemp@gmail.com>
2025-02-23 00:59:42 -0800

    objectivemanager stuff. haven't made an objective prefab yet.

--- Quartermaster/Assets/Scripts/MapObjectiveScripts/ObjectiveManager_SCRIPT.cs
+++ Quartermaster/Assets/Scripts/MapObjectiveScripts/ObjectiveManager_SCRIPT.cs
@@ -13,26 +13,48 @@ public class ObjectiveManager : NetworkBehaviour {
             DontDestroyOnLoad(gameObject);
         } else {
             Destroy(gameObject);
+
         }
     }
+
     public List<GameObject> objectivesList = new List<GameObject>();
-    private NetworkList<NetworkObjectReference> n_remainingObjectives = new NetworkList<NetworkObjectReference>();
+
+    private NetworkList<NetworkObjectReference> n_objectiveList = new NetworkList<NetworkObjectReference>();
+    private NetworkList<bool> n_objectiveStatus = new NetworkList<bool>();
+
 
     #endregion
 
     void Start(){
         // Populate the dynamic remainingObjectives list with the netobjrefs of the objectivesList.
         foreach (GameObject objective in objectivesList){
-            NetworkObjectReference netObjRef = new NetworkObjectReference(objective.GetComponent<NetworkObject>());
-            n_remainingObjectives.Add(netObjRef);
+            n_objectiveStatus.Add(false);
+            n_objectiveList.Add(new NetworkObjectReference(objective.GetComponent<NetworkObject>()));
         }
     }
     // ==============================================================================================
     #region = Objectives
-    public void RemoveObjective(NetworkObjectReference netObjRef){
-        n_remainingObjectives.Remove(netObjRef);
 
-        if (n_remainingObjectives.Count == 0){
+    // An objective should call this when it detects it is cleared.
+    public void ClearObjective(NetworkObjectReference netObjRef){
+        for (int i = 0; i < n_objectiveList.Count; i++){
+            if (n_objectiveList[i].Equals(netObjRef)){
+                n_objectiveStatus[i] = true;
+                break;
+            }
+        }
+        CheckAllObjectives();
+    }
+
+    public void CheckAllObjectives(){
+        bool allObjectivesCleared = true;
+        foreach (bool status in n_objectiveStatus){
+            if (!status){
+                allObjectivesCleared = false;
+                break;
+            }
+        }
+        if (allObjectivesCleared){
             ClearedAllObjectives();
         }
     }
@@ -47,5 +69,5 @@ public class ObjectiveManager : NetworkBehaviour {
     }
 
 
-
+    #endregion
 }
