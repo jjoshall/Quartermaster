ead55610752b35061a15f053f94c86e98574094b
Nathan Shturm <exterminathan@gmail.com>
2025-02-27 01:54:03 -0800

    updated sound emitters and enemy prefabs

--- Quartermaster/Assets/Scripts/AudioScripts/SoundEmitter_SCRIPT.cs
+++ Quartermaster/Assets/Scripts/AudioScripts/SoundEmitter_SCRIPT.cs
@@ -1,11 +1,19 @@
+using Unity.Netcode;
 using UnityEngine;
 
 public class SoundEmitter : MonoBehaviour {
     public AudioSource audioSource;
-    public void PlaySound(AudioClip clip) {
-        if (audioSource != null && clip != null) {
-            audioSource.clip = clip;
-            audioSource.Play();
+    private NetworkAudio networkAudio;
+
+    private void Awake() {
+        networkAudio = GetComponent<NetworkAudio>(); 
+    }
+
+    public void PlayNetworkedSound(string soundAddressableKey, Vector3 soundPosition) {
+        if (networkAudio != null) {
+            networkAudio.RequestSoundServerRpc(soundAddressableKey, soundPosition);
+        } else {
+            Debug.LogError("[SoundEmitter] No NetworkAudio component found!");
         }
     }
 }
