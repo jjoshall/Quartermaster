2b46aa43a7621f0764e041540386deba2d35a2be
Josh Hall <148017202+jjoshall@users.noreply.github.com>
2025-03-11 22:10:37 -0700

    commented enemy variables out in game manager

--- Quartermaster/Assets/Scripts/GameManager.cs
+++ Quartermaster/Assets/Scripts/GameManager.cs
@@ -141,25 +141,25 @@ public class GameManager : NetworkBehaviour {
     public float DmgSpec_MultiplierPer => _dmgSpec_MultiplierPer;
     public int DmgSpec_StackLimit => _dmgSpec_StackLimit;
 
-    public float MeleeEnemy_Health => meleeEnemy_Health;
-    public float MeleeEnemy_AttackCooldown => meleeEnemy_AttackCooldown;
-    public float MeleeEnemy_AttackRange => meleeEnemy_AttackRange;
-    public int MeleeEnemy_AttackDamage => meleeEnemy_AttackDamage;
-    public float MeleeEnemy_AttackRadius => meleeEnemy_AttackRadius;
-    public bool MeleeEnemy_UseGlobalTarget => meleeEnemy_UseGlobalTarget;
+    //public float MeleeEnemy_Health => meleeEnemy_Health;
+    //public float MeleeEnemy_AttackCooldown => meleeEnemy_AttackCooldown;
+    //public float MeleeEnemy_AttackRange => meleeEnemy_AttackRange;
+    //public int MeleeEnemy_AttackDamage => meleeEnemy_AttackDamage;
+    //public float MeleeEnemy_AttackRadius => meleeEnemy_AttackRadius;
+    //public bool MeleeEnemy_UseGlobalTarget => meleeEnemy_UseGlobalTarget;
 
-    public float RangedEnemy_Health => rangedEnemy_Health;
-    public float RangedEnemy_AttackCooldown => rangedEnemy_AttackCooldown;
-    public float RangedEnemy_AttackRange => rangedEnemy_AttackRange;
-    public int RangedEnemy_AttackDamage => rangedEnemy_AttackDamage;
-    public bool RangedEnemy_UseGlobalTarget => rangedEnemy_UseGlobalTarget;
+    //public float RangedEnemy_Health => rangedEnemy_Health;
+    //public float RangedEnemy_AttackCooldown => rangedEnemy_AttackCooldown;
+    //public float RangedEnemy_AttackRange => rangedEnemy_AttackRange;
+    //public int RangedEnemy_AttackDamage => rangedEnemy_AttackDamage;
+    //public bool RangedEnemy_UseGlobalTarget => rangedEnemy_UseGlobalTarget;
 
-    public float ExplosiveEnemy_Health => explosiveEnemy_Health;
-    public float ExplosiveEnemy_AttackCooldown => explosiveEnemy_AttackCooldown;
-    public float ExplosiveEnemy_AttackRange => explosiveEnemy_AttackRange;
-    public int ExplosiveEnemy_AttackDamage => explosiveEnemy_AttackDamage;
-    public float ExplosiveEnemy_AttackRadius => explosiveEnemy_AttackRadius;
-    public bool ExplosiveEnemy_UseGlobalTarget => explosiveEnemy_UseGlobalTarget;
+    //public float ExplosiveEnemy_Health => explosiveEnemy_Health;
+    //public float ExplosiveEnemy_AttackCooldown => explosiveEnemy_AttackCooldown;
+    //public float ExplosiveEnemy_AttackRange => explosiveEnemy_AttackRange;
+    //public int ExplosiveEnemy_AttackDamage => explosiveEnemy_AttackDamage;
+    //public float ExplosiveEnemy_AttackRadius => explosiveEnemy_AttackRadius;
+    //public bool ExplosiveEnemy_UseGlobalTarget => explosiveEnemy_UseGlobalTarget;
 
     #endregion
 
