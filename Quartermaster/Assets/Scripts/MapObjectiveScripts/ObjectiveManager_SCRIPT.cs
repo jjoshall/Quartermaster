f1585cdaa0b785336ba306b8275f433462a445ad
Jkumar135 <139039476+Jkumar135@users.noreply.github.com>
2025-05-12 15:18:51 -0700

    objective waypoint

--- Quartermaster/Assets/Scripts/MapObjectiveScripts/ObjectiveManager_SCRIPT.cs
+++ Quartermaster/Assets/Scripts/MapObjectiveScripts/ObjectiveManager_SCRIPT.cs
@@ -170,13 +170,11 @@ public class ObjectiveManager : NetworkBehaviour {
         n_objectivesToWin.Value--;
         n_minPerObjective[randType]--;
 
-        Debug.Log("randvalid" + randValid);
-
         if (randType == 0) {
-            taskList.text += " - Deliver the item to the mailbox. " + $"<size=1%>{randValid}</size>" + "\n";
+            taskList.text += "-Deliver the item to the mailbox. " + $"<size=1%>{randValid}</size>" + "\n";
         }
         else if (randType == 1) {
-            taskList.text += " - Locate the node and defend it from enemies for 15 seconds. " + $"<size=1%>{randValid}</size>" + "\n";
+            taskList.text += "-Locate and defend the node! " + $"<size=1%>{randValid}</size>" + "\n";
         }
         // taskList.text += minPerObjective[randType].objectivePrefab + "\n";
     }
@@ -219,8 +217,7 @@ public class ObjectiveManager : NetworkBehaviour {
     #region = Objectives
     [ServerRpc(RequireOwnership = false)]
     public void ClearObjectiveServerRpc(NetworkObjectReference refe, int index){
-        Debug.Log("index" + index);
-        taskList.text = taskList.text.Replace(index.ToString(), $"<size=100%> Objective Complete!</size>");
+        taskList.text = taskList.text.Replace(index.ToString(), $"<size=100%> Complete!</size>");
         if (!IsServer) return;
         if (!refe.TryGet(out NetworkObject netObj)){
             Debug.LogError("ObjectiveManager: ClearObjective() netObj is null.");
