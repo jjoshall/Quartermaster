b79e9a058536c06251a35738c76cdc77638fd352
Nz <evoxtemp@gmail.com>
2025-05-19 18:05:24 -0700

    late join sync held items fix

---
+++ Quartermaster/Assets/Scripts/ItemScripts/ChestSpawnPoint_SCRIPT.cs
@@ -0,0 +1,127 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Netcode;
+
+
+public class ChestSpawnPoint : NetworkBehaviour
+{
+    [Serializable]
+    public struct dropTableStruct
+    {
+        public List<itemStruct> dropTable;
+        public int weight;
+    }
+
+
+    [Header("Scene Settings (const)")]
+    [Tooltip("Assign prefab for item chests"), SerializeField]                  private GameObject _chestPrefab;
+    [Tooltip("Random if true, else cycles through list"), SerializeField]       private bool _GetRandom = false;                 // if true, get random; else cycle through. 
+    [Tooltip("Will spawn a chest every time a client joins. Overrides below setting."), SerializeField]   private bool _SpawnChestOnNetworkSpawn = false;
+    [Tooltip("Spawn chest once on server start"), SerializeField]               private bool _SpawnChestOnlyOnceOnServer = false; 
+
+    [SerializeField] private List<dropTableStruct> _possibleDropTableList = new List<dropTableStruct>();
+
+    #region Runtime
+    #endregion
+    private NetworkVariable<int> _currentDropTableIndex = new NetworkVariable<int>(0, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public override void OnNetworkSpawn()
+    {
+        if (_SpawnChestOnNetworkSpawn)
+            SpawnChestAtSelfServerRpc();
+        else if (_SpawnChestOnlyOnceOnServer && IsServer)
+            SpawnChestAtSelfServerRpc();
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    public void SpawnChestAtSelfServerRpc()
+    {
+        if (!IsServer)
+        {
+            Debug.LogError("SpawnChestAtSelf: This function should only be called on the server.");
+            return;
+        }
+        // Spawn the chest prefab at the current position and rotation of this object
+        GameObject chest = Instantiate(_chestPrefab, transform.position, transform.rotation);
+        SpawnNetObject(chest);
+        InitDropTable(chest);
+    }
+
+    private void InitDropTable(GameObject chest)
+    {
+        // Initialize the drop table on the chest
+        ItemChest_SCRIPT itemChest = chest.GetComponent<ItemChest_SCRIPT>();
+        if (itemChest != null)
+        {
+            List<itemStruct> dropTable = _GetRandom ? GrabRandomDropTable() : GrabNextDropTable();
+            itemChest.InitDropTable(dropTable);
+        }
+        else
+        {
+            Debug.LogError("ItemChest_SCRIPT component not found on chest prefab.");
+        }
+    }
+
+    private List<itemStruct> GrabRandomDropTable()
+    {
+        int totalWeight = 0;
+        foreach (var dropTable in _possibleDropTableList)
+        {
+            totalWeight += dropTable.weight;
+        }
+        int randomValue = UnityEngine.Random.Range(0, totalWeight);
+        // grab a random drop table using randomValue
+        foreach (var dropTable in _possibleDropTableList)
+        {
+            if (randomValue < dropTable.weight)
+            {
+                return dropTable.dropTable;
+            }
+            randomValue -= dropTable.weight;
+        }
+        Debug.LogError("No drop table found for random value: " + randomValue);
+        return null; // or return a default drop table
+    }
+
+    private List<itemStruct> GrabNextDropTable()
+    {
+        // grab the next drop table in the list
+        int index = _currentDropTableIndex.Value;
+        if (index >= _possibleDropTableList.Count)
+        {
+            index = 0;
+        }
+        IncrementDropTableIndexServerRpc();
+        return _possibleDropTableList[index].dropTable;
+    }
+
+    private void SpawnNetObject(GameObject obj)
+    {
+        var netObj = obj.GetComponent<NetworkObject>();
+        if (netObj != null)
+        {
+            netObj.Spawn();
+        }
+        else
+        {
+            Debug.LogError("NetworkObject component not found on object.");
+        }
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void IncrementDropTableIndexServerRpc()
+    {
+        if (!IsServer)
+        {
+            Debug.LogError("IncrementDropTableIndex: This function should only be called on the server.");
+            return;
+        }
+        _currentDropTableIndex.Value = _currentDropTableIndex.Value + 1;
+        if (_currentDropTableIndex.Value >= _possibleDropTableList.Count)
+        {
+            _currentDropTableIndex.Value = 0;
+        }
+    }
+}
--- Quartermaster/Assets/Scripts/ItemScripts/Items/MonoItem.cs
+++ Quartermaster/Assets/Scripts/ItemScripts/Items/MonoItem.cs
@@ -17,13 +17,8 @@ public abstract class Item : NetworkBehaviour
     [Tooltip("Max stack size")] public int StackLimit = 1;
     [Tooltip("Weight per unit of item")] public float weight = 0.0f;
     [Tooltip("Current stack quantity, also modified during runtime")] public int quantity = 1;
-    private NetworkVariable<int> n_syncedQuantity = new NetworkVariable<int>(1, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
     [Tooltip("Item icon")] public Texture icon = null;
     [Tooltip("OnUse sound emitters")] public SoundEmitter[] soundEmitters = null;
-    [HideInInspector] public bool IsPickedUp = false;
-    [HideInInspector] public NetworkVariable<bool> n_isPickedUp = new NetworkVariable<bool>(false, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
-    [HideInInspector] public NetworkVariable<bool> n_isCurrentlySelected = new NetworkVariable<bool>(false, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
-    [HideInInspector] public GameObject attachedWeaponSlot = null;
     // [HideInInspector]                                                       public NetworkVariable<bool> CurrentlySelected = new NetworkVariable<bool>(false, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
 
     #endregion
@@ -36,6 +31,12 @@ public abstract class Item : NetworkBehaviour
     #region RUNTIME
     // RUNTIME VARIABLES ===================================================================================================
     [HideInInspector] public GameObject userRef;
+    [HideInInspector] private NetworkVariable<NetworkObjectReference> n_userRef = new NetworkVariable<NetworkObjectReference>(default, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
+    [HideInInspector] public bool IsPickedUp = false;
+    [HideInInspector] public NetworkVariable<bool> n_isPickedUp = new NetworkVariable<bool>(false, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
+    [HideInInspector] public NetworkVariable<bool> n_isCurrentlySelected = new NetworkVariable<bool>(false, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
+    [HideInInspector] public GameObject attachedWeaponSlot = null;
+    private NetworkVariable<int> n_syncedQuantity = new NetworkVariable<int>(1, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
     public float lastUsed
     {
         get => userRef.GetComponent<PlayerStatus>().GetLastUsed(uniqueID);
@@ -68,7 +69,7 @@ public abstract class Item : NetworkBehaviour
 
     #region FUNCTIONS
     // FUNCTION ABSTRACTS ===============================================================================================
-    public virtual void OnNetworkSpawn()
+    public override void OnNetworkSpawn()
     {
         // Called when the item is spawned on the server.
         Debug.Log("Spawned item: " + gameObject.name);
@@ -135,7 +136,20 @@ public abstract class Item : NetworkBehaviour
     // Sets server quantity to local quantity (passed as parameter).
     public void SyncServerNetVarToLocal()
     {
-        SyncServerToLocalServerRpc(quantity, IsPickedUp);
+        if (userRef == null)
+        {
+            SyncServerToLocalServerRpc(quantity, IsPickedUp);
+        }
+        else
+        {
+            var userNetObj = userRef.GetComponent<NetworkObject>();
+            if (userNetObj == null)
+            {
+                Debug.LogError("SyncServerNetVarToLocal: userRef is null.");
+                return;
+            }
+            SyncServerToLocalServerRpc(quantity, IsPickedUp, userNetObj);
+        }
     }
     [ServerRpc(RequireOwnership = false)]
     private void SyncServerToLocalServerRpc(int quantity, bool isPickedUp)
@@ -143,13 +157,25 @@ public abstract class Item : NetworkBehaviour
         n_syncedQuantity.Value = quantity;
         n_isPickedUp.Value = isPickedUp;
     }
+    [ServerRpc(RequireOwnership = false)]
+    private void SyncServerToLocalServerRpc(int quantity, bool isPickedUp, NetworkObjectReference userNetObj)
+    {
+        // Sync the local variables to the server.
+        n_syncedQuantity.Value = quantity;
+        n_isPickedUp.Value = isPickedUp;
+        n_userRef.Value = userNetObj;
+    }
 
 
     // Sets all client quantities to server quantity.
     public void SyncClientsToServer()
     {
+        // Sync self.
         quantity = n_syncedQuantity.Value;
         IsPickedUp = n_isPickedUp.Value;
+        userRef = GetGameObjectFromNetObj(n_userRef.Value);
+        attachedWeaponSlot = GetWeaponSlot(userRef);
+        // Sync other clients to server.
         SyncLocalToServerServerRpc();
     }
     [ServerRpc(RequireOwnership = false)]
@@ -160,12 +186,58 @@ public abstract class Item : NetworkBehaviour
     [ClientRpc]
     private void SyncLocalToServerClientRpc()
     {
+
+        Debug.Log("Syncing local to server for item: " + uniqueID);
         quantity = n_syncedQuantity.Value;
         IsPickedUp = n_isPickedUp.Value;
         bool hideItem = (!n_isCurrentlySelected.Value && IsPickedUp) ? true : false;
         foreach (Renderer r in GetComponentsInChildren<Renderer>())
         {
             r.enabled = !hideItem;
+        }
+        userRef = GetGameObjectFromNetObj(n_userRef.Value);
+        attachedWeaponSlot = GetWeaponSlot(userRef);
+    }
+
+    #endregion
+
+
+
+
+
+
+    #region HELPERS
+    private GameObject GetWeaponSlot(GameObject player)
+    {
+        if (player == null)
+        {
+            Debug.Log("GetWeaponSlot: player is null.");
+            return null;
+        }
+        Inventory inv = player.GetComponent<Inventory>();
+        if (inv == null)
+        {
+            Debug.LogError("GetWeaponSlot: Inventory component not found on player.");
+            return null;
+        }
+        GameObject weaponSlot = inv.weaponSlot;
+        if (weaponSlot == null)
+        {
+            Debug.LogError("GetWeaponSlot: Weapon slot not found.");
+            return null;
+        }
+        return weaponSlot;
+    }
+    private GameObject GetGameObjectFromNetObj(NetworkObjectReference netObj)
+    {
+        if (netObj.TryGet(out NetworkObject obj))
+        {
+            return obj.gameObject;
+        }
+        else
+        {
+            Debug.Log("GetGameObjectFromNetObj: Could not get game object from network object reference. UserRef set null.");
+            return null;
         }
     }
     #endregion
