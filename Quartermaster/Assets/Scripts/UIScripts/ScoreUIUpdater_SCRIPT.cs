24818e7d93d59a3e1c194633291077a196569307
Josh Hall <148017202+jjoshall@users.noreply.github.com>
2025-05-20 16:00:39 -0700

    one more change

--- Quartermaster/Assets/Scripts/UIScripts/ScoreUIUpdater_SCRIPT.cs
+++ Quartermaster/Assets/Scripts/UIScripts/ScoreUIUpdater_SCRIPT.cs
@@ -5,6 +5,7 @@ using Unity.Netcode;
 public class ScoreUIUpdater_SCRIPT : NetworkBehaviour
 {
     [SerializeField] private TextMeshProUGUI scoreNum;
+    [SerializeField] private TextMeshProUGUI killsNum;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,12 +14,9 @@ public class ScoreUIUpdater_SCRIPT : NetworkBehaviour
 
         GameManager.instance.totalScore.OnValueChanged += OnScoreChanged;
         UpdateScore(GameManager.instance.totalScore.Value);
-    }
 
-    private void Update() {
-        if (Input.GetKeyDown(KeyCode.F)) {
-            Debug.Log("Current score: " + GameManager.instance.totalScore.Value);
-        }
+        GameManager.instance.totalEnemyKills.OnValueChanged += OnKillsChanged;
+        UpdateKills(GameManager.instance.totalEnemyKills.Value);
     }
 
     //private void OnDestroy() {
@@ -31,9 +29,20 @@ public class ScoreUIUpdater_SCRIPT : NetworkBehaviour
         UpdateScore(newValue);
     }
 
+    private void OnKillsChanged(int oldValue, int newValue) {
+        UpdateKills(newValue);
+    }
+
     private void UpdateScore(int score) {
         if (scoreNum != null) {
             scoreNum.text = $"{score}";
+        }
+    }
+
+    private void UpdateKills(int kills) {
+        if (killsNum != null) {
+            Debug.Log($"Kills: {kills}");
+            killsNum.text = $"{kills}";
         }
     }
 }
