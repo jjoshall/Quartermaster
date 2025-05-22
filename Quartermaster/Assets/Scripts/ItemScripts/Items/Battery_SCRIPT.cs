ab66ff598cd5205efcbab0c20fb7359e31da7791
Nz <evoxtemp@gmail.com>
2025-05-21 23:04:02 -0700

    wip

--- Quartermaster/Assets/Scripts/ItemScripts/Items/Battery_SCRIPT.cs
+++ Quartermaster/Assets/Scripts/ItemScripts/Items/Battery_SCRIPT.cs
@@ -10,21 +10,25 @@ public class Battery : Item
     public Material emptyBatteryMaterial;
     public Material fullBatteryMaterial;
 
-    #region Battery Item Settings
+    #region Settings
     #endregion
-    [Header("Adjustable per battery type")]
-    public float maxChargeTime = 30f;
-    public float maxChargeCapacity = 90f;
-    public float chargeLeakRate = 0.5f;
-    public float serverSyncInterval = 10.0f; // syncs charge from server every interval.
+    [SerializeField] private float _chargeRate = 3f;
+    [SerializeField] private float _maxChargeCapacity = 90f;
+    [SerializeField] private float _chargingLeakRate = 0.5f;
+    [SerializeField] private float _idleLeakRate = 0.1f;
+    [SerializeField] private float _serverSyncInterval = 10.0f; // syncs charge from server every interval.
 
-    #region RuntimeVars
-    #endregion
+    #region RuntimeLocal
+    #endregion // update()
     private float _currentCharge = 0f;              // high frequency updates. update locally. periodic / event-based syncing from server.
-    private float _currentChargeTime = 0f;          // high frequency updates. update locally. periodic / event-based syncing from server.
+    private int _litSegments = 0;                   // _currentCharge / _maxChargeCapacity * batterySegments.Count; rounded.
     private float _serverSyncTimer = 0f;
-    private NetworkVariable<int> _litSegments = new NetworkVariable<int>(0); // periodic / event-based updates.
+    // ischarging should allow server write
 
+    #region RuntimeNetwork
+    #endregion // updates periodically
+    private NetworkVariable<bool> _isCharging = new NetworkVariable<bool>(false);
+    private NetworkVariable<bool> _hasBeenFilled = new NetworkVariable<bool>(false); // true if battery has been filled at least once.
 
 
     public override void OnButtonUse(GameObject user)
@@ -41,23 +45,48 @@ public class Battery : Item
     #endregion
     public void Update()
     {
-        // update _currentCharge
+        if (_isCharging.Value) Charge();
+        if (!_hasBeenFilled.Value && _currentCharge > 0) Leak();
+        UpdateLitSegmentCount();
+        UpdateSegmentDisplay();
     }
 
-    public void SyncChargeToServer()
+    public void Charge()
+    {
+        if (_currentCharge < _maxChargeCapacity)
+        {
+            _currentCharge = Mathf.Clamp(_currentCharge + Time.deltaTime * _chargeRate, 0, _maxChargeCapacity);
+        }
+        else
+        {
+            // battery is full. do nothing.
+        }
+    }
+
+    public void Leak()
     {
         
     }
 
-    [ServerRpc(RequireOwnership = false)]
-    
+    public void UpdateLitSegmentCount()
+    {
+        // update lit segments
+        if (_currentCharge > 0)
+        {
+            _litSegments = Mathf.RoundToInt(_currentCharge / _maxChargeCapacity * batterySegments.Count);
+        }
+        else
+        {
+            _litSegments = 0;
+        }
+    }
 
     public void UpdateSegmentDisplay()
     {
         // update battery segments
         for (int i = 0; i < batterySegments.Count; i++)
         {
-            if (i < _litSegments.Value)
+            if (i < _litSegments)
             {
                 batterySegments[i].GetComponent<MeshRenderer>().material = fullBatteryMaterial;
             }
@@ -71,5 +100,17 @@ public class Battery : Item
 
     #region NetworkVarWriteHelpers
     #endregion
+
+    // only call this periodically (charge variables managed locally to avoid network bloat)
+    public void SyncChargeToServer()
+    {
+
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    public void SetChargingStatusServerRpc(bool isCharging)
+    {
+        _isCharging.Value = isCharging;
+    }
 
 }
